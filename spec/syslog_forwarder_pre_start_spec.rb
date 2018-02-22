53d5f2c3c771bf27b1446a91854f3bb5e740e911
Josh Hill <jhill@pivotal.io>
2018-02-22 17:24:14 +0000

    Refactor syslog_forwarder pre-start script spec
    Use official Bosh::Template::Test helpers

--- spec/syslog_forwarder_pre_start_spec.rb
+++ spec/syslog_forwarder_pre_start_spec.rb
@@ -1,53 +1,45 @@
-require 'json'
-require 'yaml'
-require 'helpers/bosh_template'
+require 'bosh/template/test'
 
 RSpec.describe 'syslog_forwarder pre-start' do
-  TEMPLATE_PATH = 'jobs/syslog_forwarder/templates/pre-start.erb'
-  JOB_NAME = 'syslog_forwarder'
-  MINIMUM_MANIFEST = <<~MINIMUM_MANIFEST
-  instance_groups:
-  - name: syslog_forwarder
-    jobs:
-    - name: syslog_forwarder
-  MINIMUM_MANIFEST
-  NO_LINKS = {}
+  before(:all) do
+    release_path = File.join(File.dirname(__FILE__), '..')
+    release = Bosh::Template::Test::ReleaseDir.new(release_path)
+    job = release.job('syslog_forwarder')
+    @template = job.template('bin/pre-start')
+  end
 
   it 'does not delete any files' do
-    manifest = generate_manifest_cleanup_conf_files
-    actual_template = BoshTemplate.render(TEMPLATE_PATH, JOB_NAME, manifest, NO_LINKS)
-    expect(actual_template).not_to include('rm -f /etc/rsyslog.d')
+    pre_start_script = @template.render({})
+    expect(pre_start_script).not_to include('rm -f')
   end
 
   it 'deletes files in /etc/rsyslog.d' do
-    manifest = generate_manifest_cleanup_conf_files('00-default.conf', '01-custom.conf')
-    actual_template = BoshTemplate.render(TEMPLATE_PATH, JOB_NAME, manifest, NO_LINKS)
-    expect(actual_template).to include('rm -f /etc/rsyslog.d/00-default.conf')
-    expect(actual_template).to include('rm -f /etc/rsyslog.d/01-custom.conf')
+    properties = job_properties(cleanup_conf_files: [
+      '00-default.conf',
+      '01-custom.conf'
+    ])
+    pre_start_script = @template.render(properties)
+    expect(pre_start_script).to include('rm -f /etc/rsyslog.d/00-default.conf')
+    expect(pre_start_script).to include('rm -f /etc/rsyslog.d/01-custom.conf')
   end
 
   it 'does not delete files outside /etc/rsyslog.d' do
-    manifest = generate_manifest_cleanup_conf_files('../../var/log/syslog',
-      '../../var/vcap/store/my_data.yml', '00-default.conf')
-    actual_template = BoshTemplate.render(TEMPLATE_PATH, JOB_NAME, manifest, NO_LINKS)
-    expect(actual_template).to include('rm -f /etc/rsyslog.d/00-default.conf')
-    expect(actual_template).not_to include('/var/log/syslog')
-    expect(actual_template).not_to include('/var/vcap/store/my_data.yml')
+    properties = job_properties(cleanup_conf_files: [
+      '../../var/log/syslog',
+      '../../var/vcap/store/my_data.yml',
+      '00-default.conf'
+    ])
+    pre_start_script = @template.render(properties)
+    expect(pre_start_script).to include('rm -f /etc/rsyslog.d/00-default.conf')
+    expect(pre_start_script).not_to include('/var/log/syslog')
+    expect(pre_start_script).not_to include('/var/vcap/store/my_data.yml')
   end
-end
 
-def generate_manifest(minimum_manifest)
-  manifest = YAML.load(minimum_manifest)
-  yield(manifest) if block_given?
-  manifest
-end
-
-def generate_manifest_cleanup_conf_files(*filenames)
-  generate_manifest(MINIMUM_MANIFEST) do |manifest|
-    manifest['instance_groups'][0]['jobs'][0]['properties'] = {
+  def job_properties(cleanup_conf_files:)
+    {
       'syslog' => {
         'migration' => {
-          'cleanup_conf_files' => filenames
+          'cleanup_conf_files' => cleanup_conf_files
         }
       }
     }
